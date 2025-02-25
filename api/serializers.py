769c4637284aa304a682cb8b1f0ed0f903c04c01
Hillel Arnold <hillel.arnold@gmail.com>
2025-02-25 16:20:32 -0500

    correct fields

--- api/serializers.py
+++ api/serializers.py
@@ -6,10 +6,10 @@ from .models import Event, Package
 class PackageSerializer(HyperlinkedModelSerializer):
     class Meta:
         model = Package
-        fields = ['__all__']
+        fields = '__all__'
 
 
 class EventSerializer(HyperlinkedModelSerializer):
     class Meta:
         model = Event
-        fields = ['__all__']
+        fields = '__all__'
