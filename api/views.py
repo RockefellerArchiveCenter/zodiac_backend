f1a33d9670159fa183046a340e9f277edda81e5d
Hillel Arnold <hillel.arnold@gmail.com>
2025-02-25 16:45:15 -0500

    add order to querysets

--- api/views.py
+++ api/views.py
@@ -9,7 +9,7 @@ from .serializers import (EventSerializer, PackageEventSerializer,
 
 
 class PackageViewSet(ModelViewSet):
-    queryset = Package.objects.all()
+    queryset = Package.objects.all().order_by('-created')
     serializer_class = PackageSerializer
 
     def get_serializer_class(self):
@@ -21,10 +21,10 @@ class PackageViewSet(ModelViewSet):
     def events(self, request, pk=None):
         """Show events related to a package."""
         package = get_object_or_404(Package, pk=pk)
-        serializer = PackageEventSerializer(package.event_set.all(), many=True)
+        serializer = PackageEventSerializer(package.event_set.all().order_by('created'), many=True)
         return Response(serializer.data)
 
 
 class EventViewSet(ModelViewSet):
-    queryset = Event.objects.all()
+    queryset = Event.objects.all().order_by('-created')
     serializer_class = EventSerializer
