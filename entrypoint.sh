c72dd4e8132b7f6f0752693c0e2ccb70b0d7dc8d
Hillel Arnold <hillel.arnold@gmail.com>
2025-03-05 14:03:03 -0500

    copy config file on startup, correct db service name

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# Copy config file if it doesn't exist
+if [ ! -f aurora/config.py ]; then
+    echo "Copying example config file"
+    cp zodiac_backend/config.py.example zodiac_backend/config.py
+fi
+
 # Apply database migrations
 echo "Apply database migrations"
 ./wait-for-it.sh db:5432 -- python manage.py migrate
