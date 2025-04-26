e7b8df541ce7c1b0114d26d03a6a1309a783c71f
Hillel Arnold <hillel.arnold@gmail.com>
2025-04-26 13:40:03 -0400

    get request data correctly

--- api/views.py
+++ api/views.py
@@ -51,8 +51,8 @@ class RestartServiceView(APIView):
 
     def post(self, request, **kwargs):
         try:
-            service = request.POST['service']
-            package_id = request.POST['package_id']
+            service = request.data['service']
+            package_id = request.data['package_id']
         except KeyError:
             return Response("Post data must include both service and package_id.", status=500)
 
